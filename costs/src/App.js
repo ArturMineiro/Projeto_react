676e77e4681fa3249bda31bd2ee07e3884c37fb3
Artur Mineiro <105992706+ArturMineiro@users.noreply.github.com>
2024-05-12 10:49:29 -0300

    Primeiros components e css ´para projeto
    Primeiros components e css ´para projeto

--- costs/src/App.js
+++ costs/src/App.js
@@ -1,10 +1,36 @@
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from "./components/pages/Home";
+import Contact from "./components/pages/Contact";
+import Company from "./components/pages/Company";
+import NewProject from "./components/pages/NewProject";
+import Container from "./layont/Container";
 
 function App() {
   return (
-    <div className="App">
-     <h1>Costs</h1>
-    </div>
+    <Router>
+      <Container customClass="min-height">
+      <div>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/company">Compania</Link></li>
+          <li><Link to="/contact">Contato</Link></li>
+          <li><Link to="/newproject">Novo projeto</Link></li>
+        </ul>
+
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/company" element={<Company />} />
+          <Route exact path="/contact" element={<Contact />} />
+          <Route exact path="/newproject" element={<NewProject />} />
+        </Routes>
+      </div>
+      </Container>
+    </Router>
   );
 }
 

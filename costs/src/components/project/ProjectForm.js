aae3a276e63b38e665f25a0117cfc9e29263e4fb
artur.mineiro <arturmineiro2@hotmail.com>
2024-05-16 15:48:05 -0300

    select com dados de um banco fake para alimenta-lo // useEffect e fetch

--- costs/src/components/project/ProjectForm.js
+++ costs/src/components/project/ProjectForm.js
@@ -1,3 +1,5 @@
+import {useEffect, useState} from 'react'
+
 import styles from './ProjectForm.module.css'
 import Input from '../form/Input'
 import Select from '../form/Select'
@@ -5,6 +7,23 @@ import SubmitButton from '../form/SubmitButton'
 
 
 function ProjectForm({btnText}){
+
+  const [categories,setCategories] = useState([])
+
+ useEffect(( ) => {
+  fetch("http://localhost:5000/categories", {
+    method: 'Get',
+    headers:{
+      'Content-type': 'aplication/json',
+    },
+    
+      })
+      .then((resp) => resp.json())
+      .then((data)=> {
+        setCategories(data)
+      })
+      .catch((err) => console.log(err))
+ })
 return(
   <form className={styles.form}>
     <div>
@@ -13,7 +32,7 @@ return(
     <div>
     <Input type="Number" text="orçamento do projeto" name="budget" placeholder="insira o orçamento total"/>
 </div>
-<Select name="category_id" text="Selecione a categoria"/>
+<Select name="category_id" text="Selecione a categoria" options={categories}/>
 <div>
    <SubmitButton text={btnText} />
 </div>

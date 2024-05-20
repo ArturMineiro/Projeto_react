bcbfe39c3067393bace54fb31a361c77c08fdc33
artur.mineiro <arturmineiro2@hotmail.com>
2024-05-20 15:36:22 -0300

    Resgatando projetos do banco de dados

--- costs/src/components/pages/Projects.js
+++ costs/src/components/pages/Projects.js
@@ -4,15 +4,31 @@ import {useLocation} from 'react-router-dom'
 import styles from './Projects.module.css'
 import Container from "../layont/Container"
 import LinkButton from "../layont/LinkButton"
-
-
+import ProjectCard from "../project/ProjectCard"
+import { useState,useEffect } from "react"
 
 function Projects(){
-const location = useLocation()
+
+    const [projects,setProjects] = useState([])
+    const location = useLocation()
 let message='Projeto criado com sucesso!!'
 if(location.state){
     message =location.state.message
 }
+useEffect(() => {
+    fetch('http://localhost:5000/projects', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+
+    }).then(resp => resp.json())
+    .then(data => {
+   console.log(data)
+        setProjects(data)
+    })
+.catch((err) => console.log(err))
+},[])
     return( 
     <div className={styles.project_container}>
         <div className={styles.title_container}>
@@ -21,7 +37,10 @@ if(location.state){
         </div>
   {message && <Message type="success" msg={message}/>}
  <Container  customClass="start">
- <p>Projetos....</p>
+{projects.length >0 && 
+projects.map((project) => (
+    <ProjectCard id={project.id} name={project.name} budget={project.budget} category={project.category.name} key={project.id} /> 
+))}
  </Container>
     </div>
     )
---
+++ costs/src/components/project/ProjectCard.js
@@ -0,0 +1,30 @@
+import styles from './ProjectCard.module.css'
+import {BePencil, BsFillTrashFill, BsPencil} from 'react-icons/bs'
+import {Link} from 'react-router-dom'
+function ProjectCard({id, name, budget, category,handleRemove}){
+  return (
+    <div className={styles.project_card}> 
+        <h4>Orçamento</h4>
+    <p><span>
+        Orçamento:
+        </span>
+        R${budget}
+        </p>
+        <p className={styles.category_text}>
+        <span className={`${styles[category.toLowerCase()]}`}></span>{category}
+
+        </p>
+        <div className={styles.project_card_actions}>
+            <Link to='/'>
+                <BsPencil/>Editar
+            </Link>
+    <button>
+        <BsFillTrashFill/> Excluir
+        </button>
+        </div>
+    </div>
+  )
+
+}
+
+export default ProjectCard
